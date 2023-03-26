69a994e9c6c64860d05a57c573a09464067ffab9
Agnieszka Kruczek <agnieszka1.kruczek@gmail.com>
2023-03-26 19:37:31 +0200

    Change in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,19 @@
-//Hello, this is a new comment
+#include <iostream>
+#include "header.hpp"
+
+using namespace std;
+
+int main(){
+cout << "Addition and substraction" << endl;
+
+cout << "Enter first number: " << endl;
+float a;
+cin >> a;
+cout << "Enter second number: " << endl;
+float b;
+cin >> b;
+cout << "The result of addition is " << add(a,b) << endl;
+cout << "The result of substraction is " << sub(a,b) << endl;
+
+return 0;
+}
